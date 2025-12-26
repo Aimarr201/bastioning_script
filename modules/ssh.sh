2c276ff355b025dc650c7e50f0a9bd18e5dc8700
Aimar <mendibil.ayo@gmail.com>
2025-12-27 00:27:52 +0100

    prueba de mfa

--- modules/ssh.sh
+++ modules/ssh.sh
@@ -15,13 +15,13 @@ backup_file /etc/ssh/sshd_config
 service_start ssh
 service_enable ssh
 
-google-authenticator -t -C -f -q -e 5 -Q NONE -d -w 3 -r 3 -R 30
+sudo -u $SSH_USERNAME google-authenticator -t -C -f -q -e 5 -Q NONE -d -w 3 -r 3 -R 30
 
 cat >> /home/$SSH_USERNAME/.ssh/authorized_keys <<EOF
 $SSH_PUBLICKEY
 EOF
 
-cat >> /etc/ssh/sshd_config <<EOF
+cat > /etc/ssh/sshd_config <<EOF
 Include /etc/ssh/sshd_config.d/*.conf
 
 # Puerto SSH recomenadado cambiarlo (por defecto 22) para evitar escaneos automaticos
@@ -110,3 +110,66 @@ Subsystem sftp internal-sftp -f AUTHPRIV -l INFO
 # Permite que el cliente pase variables de entorno específicas al servidor
 AcceptEnv LANG LC_* COLORTERM NO_COLOR
 EOF
+
+cat > /etc/ssh/sshd_config <<EOF
+# Configuración PAM para el servicio Secure Shell
+
+# Autenticación de dos factores con Google Authenticator
+auth required pam_google_authenticator.so
+
+# Autenticación estándar de Un*x.
+#@include common-auth
+
+# Deshabilitar inicios de sesión no-root cuando /etc/nologin exista.
+account    required     pam_nologin.so
+
+# Descomentar y editar /etc/security/access.conf si necesitas establecer límites complejos
+# de acceso que sean difíciles de expresar en sshd_config.
+# account  required     pam_access.so
+
+# Autorización estándar de Un*x.
+@include common-account
+
+# SELinux debe ser la primera regla de sesión. Esto asegura que cualquier
+# contexto pendiente haya sido limpiado. Sin esto, es posible que un
+# módulo ejecute código en el dominio incorrecto.
+session [success=ok ignore=ignore module_unknown=ignore default=bad]        pam_selinux.so close
+
+# Establecer el atributo del proceso loginuid.
+session    required     pam_loginuid.so
+
+# Crear una nueva clave de sesión.
+session    optional     pam_keyinit.so force revoke
+
+# Configuración y cierre estándar de sesión en Un*x.
+@include common-session
+
+# Imprimir el mensaje del día tras un inicio de sesión exitoso.
+# Esto incluye una parte generada dinámicamente desde /run/motd.dynamic
+# y una parte estática (editable por el administrador) desde /etc/motd.
+session    optional     pam_motd.so  motd=/run/motd.dynamic
+session    optional     pam_motd.so noupdate
+
+# Imprimir el estado del buzón de correo del usuario tras un inicio de sesión exitoso.
+session    optional     pam_mail.so standard noenv # [1]
+
+# Configurar los límites del usuario desde /etc/security/limits.conf.
+session    required     pam_limits.so
+
+# Leer las variables de entorno desde /etc/environment y
+# /etc/security/pam_env.conf.
+session    required     pam_env.so # [1]
+# En Debian 4.0 (etch), las variables de entorno relacionadas con el locale se movieron a
+# /etc/default/locale, así que también se lee ese archivo.
+session    required     pam_env.so envfile=/etc/default/locale
+
+# SELinux necesita intervenir en el momento del inicio de sesión para asegurar que el proceso
+# comience en el contexto de seguridad adecuado. Solo las sesiones que están destinadas
+# a ejecutarse en el contexto del usuario deben ejecutarse después de esto.
+session [success=ok ignore=ignore module_unknown=ignore default=bad]        pam_selinux.so open
+
+# Actualización estándar de contraseñas de Un*x.
+@include common-password
+EOF
+
+service_restart ssh
